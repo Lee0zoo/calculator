08dd3e0743eb8e2f7293399b560c886e3580643d
Robbie <robbie331@>
2024-04-26 15:21:07 +0900

    Level2 - 3 + App의 result 수정, Calculator의 조건문을 if 문으로 수정

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,7 +16,7 @@ public class App {
         Calculator calc = new Calculator();
 
         // 결과를 저장할 리스트
-        ArrayList<Integer> rList = new ArrayList<>();
+//        ArrayList<Integer> rList = new ArrayList<>();
 
         // 특정 조건을 만족할 때까지 사용자로부터 계속 입력을 받음
         while(true) {
@@ -28,7 +28,7 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요 : ");
             char operator = sc.next().charAt(0); // charAt()을 통해 char 타입으로 연산 기호 저장
 
-            int result = 0; // 연산 결과를 담을 변수
+//            int result = 0; // 연산 결과를 담을 변수
 
 //            // 사칙연산 기호에 따라 다른 연산을 적용함
 //            switch (operator) {
@@ -52,8 +52,8 @@ public class App {
 //                    break;
 //            }
 
-            // 연산 수행
-            calc.calculate(num1, num2, operator);
+            // 연산 수행 + 결과 result에 저장
+            int result = calc.calculate(num1, num2, operator);
 
             // 결과 출력
             System.out.println("결과 : " + result);
@@ -70,25 +70,30 @@ public class App {
 //                index++;
 //            }
 
-            // 결과를 리스트에 저장
-            rList.add(result);
+//            // 결과를 리스트에 저장
+//            rList.add(result);
 
             // 연산 결과 삭제 의사를 묻는 내용 출력
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String rmv = sc.next();
 
             // 사용자가 remove를 입력했다면, 가장 먼저 입력된 연산 결과를 삭제
-            if(rmv.equals("remove")) {
-                rList.remove(0);
-            }
+//            if(rmv.equals("remove")) {
+//                rList.remove(0);
+//            }
+            calc.setRList(rmv);
 
             // 저장된 연산 결과 출력 의사를 묻는 내용 출력
             System.out.println("저장된 연산 결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String ck = sc.next();
 
             // 사용자가 inquiry를 입력했다면, 연산 결과 전부 조회
+//            if(ck.equals("inquiry")) {
+//                rList.forEach(data -> System.out.println(data + " "));
+//            }
+
             if(ck.equals("inquiry")) {
-                rList.forEach(data -> System.out.println(data + " "));
+                calc.getRList().forEach(data -> System.out.println(data + " "));
             }
 
             // 추가 연산 여부를 묻는 내용 출력
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -3,29 +3,44 @@ package calculator;
 import java.util.*;
 
 public class Calculator {
-    ArrayList<Integer> rList = new ArrayList<>();
-    int result;
+    // 외부에서 접근 불가능한 컬렉션 필드 선언 & 생성
+    private ArrayList<Integer> rList = new ArrayList<>();
 
     public int calculate(int num1, int num2, char operater) {
+        int result = 0;
         try {
-            switch (operater) {
-                case '+' : result = num1 + num2;
-                    break;
-                case '-' : result = num1 - num2;
-                    break;
-                case '*' : result = num1 * num2;
-                    break;
-                case '/' :
-                    try {
-                        result = num1 / num2;
-                    } catch (ArithmeticException e) {
-                        System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
-                    }
-                    break;
+            if(operater == '+') {
+                result = num1 + num2;
+            } else if(operater == '-') {
+                result = num1 - num2;
+            } else if(operater == '*') {
+                result = num1 * num2;
+            } else if(operater == '/') {
+                try {
+                    result = num1 / num2;
+                } catch (ArithmeticException e) {
+                    System.out.println("나눗셈 연산에서 분모(두 번째 정수)에 0이 입력될 수 없습니다.");
+                }
             }
         } catch (Exception e) {
-            System.out.println("욜바른 연산 기호를 입력해 주세요.");
+            System.out.println("올바른 연산 기호를 입력해 주세요.");
         }
+
+        // 결과를 리스트에 저장
+        rList.add(result);
+
         return result;
+    }
+
+    // Getter
+    public ArrayList<Integer> getRList() {
+        return rList;
+    }
+
+    // Setter
+    public void setRList(String rmv) {
+        if(rmv.equals("remove")) {
+            this.rList.remove(0);
+        }
     }
 }
